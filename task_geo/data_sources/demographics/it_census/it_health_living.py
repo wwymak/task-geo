499df2c5e5849c46124b8b4b8dedb029a605f5a8
wwymak <wwymak@gmail.com>
2020-04-17 21:43:45 +0100

    add missing column name to doc

--- task_geo/data_sources/demographics/it_census/it_health_living.py
+++ task_geo/data_sources/demographics/it_census/it_health_living.py
@@ -7,12 +7,13 @@ import pandas as pd
 smokers = pd.read_csv('lifestyle_metrics_data/DCCV_AVQ_PERSONE_smoking.csv')
 smokers = smokers.loc[(smokers.TIPO_DATO_AVQ == '14_FUMO_SI') & (
     smokers.Misura == 'per 100 persone con le stesse caratteristiche'), :]
-smokers = smokers.rename(columns={'Territorio': 'Region',
-                                  'Value': 'ratio_smoking'})[['Region', 'ratio_smoking']]
+smokers = smokers.rename(columns={'Territorio': 'region',
+                                  'Value': 'ratio_smoking'})[['region', 'ratio_smoking']]
 
-health = pd.read_csv('lifestyle_metrics_data/DCCV_AVQ_PERSONE_health.csv')
+health = pd.read_csv('lifestyle_metrics_data/DCCV_AVQ_PERSONE_health.csv')\
+    .rename(columns={'Territory': 'region'})
 health = health.loc[health.Measure == 'per 100 people with the same characteristics']
-health_pivot = health.pivot_table(index='Territory', values='Value', columns=['Data type'])
+health_pivot = health.pivot_table(index='region', values='Value', columns=['Data type'])
 health_col_name_mapping = {
     'chronic patients - suffering from allergic diseases': 'ratio_allergic_diseases',
     'chronic patients - suffering from chronic bronchitis': 'ratio_chronic_bronchitis',
@@ -26,13 +27,14 @@ health_col_name_mapping = {
 
 health_pivot = health_pivot.rename(columns=health_col_name_mapping).reset_index()
 
-bmi = pd.read_csv('lifestyle_metrics_data/DCCV_AVQ_PERSONE1_bmi.csv')
+bmi = pd.read_csv('lifestyle_metrics_data/DCCV_AVQ_PERSONE1_bmi.csv')\
+    .rename(columns={'Territory': 'region'})
 bmi = bmi.loc[(bmi.Measure == 'per 100 people with the same characteristics') & (
     bmi.Gender == 'total'), :]
-bmi_pivot = bmi.pivot_table(index='Territory', values='Value', columns=['Data type'])
+bmi_pivot = bmi.pivot_table(index='region', values='Value', columns=['Data type'])
 bmi_column_name_mapping = {k: f"ration_{k.replace(' ', '_')}" for k in bmi.columns}
 bmi_pivot = bmi_pivot.rename(columns=bmi_column_name_mapping).reset_index()
 
 all_conditions = (bmi_pivot
-                  .merge(health_pivot, left_on='Territory', right_on='Territory', how='left')
-                  .merge(smokers, left_on='Territory', right_on='Territory', how='left'))
+                  .merge(health_pivot, left_on='region', right_on='region', how='left')
+                  .merge(smokers, left_on='region', right_on='region', how='left'))
